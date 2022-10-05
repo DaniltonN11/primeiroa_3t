f69d41d6eb6d4ee7ab47781d198e53f66003878e
D.ART_STUDIO <90416068+DaniltonN11@users.noreply.github.com>
2022-10-05 08:20:44 -0300

    pao

--- estudo.js
+++ estudo.js
@@ -16,3 +16,13 @@ function escrever(){
     document.write(tabuada+" x 10 = "+ (tabuada*10)+"<br>");
     
 }
+
+function minhaTabuada(){
+    for(let i = 1; i <= 10; i++){ 
+        document.write(" Tabuada do "+ i + "<br>");
+        for(let j = 1; j<=10; j++){
+            document.write(i +" x " + j + "=" + (j*i)+ "<br>");
+        }
+        document.write("<vbr>");
+    }
+}
